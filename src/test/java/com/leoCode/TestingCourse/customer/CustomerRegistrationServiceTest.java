180cbd46cc4cc7574c04abe870a59f5c1a27626b
leobarrientos02 <leobarrientos02@gmail.com>
2022-08-02 11:27:14 -0400

    CustomerRepository testing

---
+++ src/test/java/com/leoCode/TestingCourse/customer/CustomerRegistrationServiceTest.java
@@ -0,0 +1,14 @@
+package com.leoCode.TestingCourse.customer;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerRegistrationServiceTest {
+
+    private CustomerRegistrationService underTest;
+
+    @BeforeEach
+    void setUp(){
+    }
+}
--- src/test/java/com/leoCode/TestingCourse/customer/CustomerRepositoryTest.java
+++ src/test/java/com/leoCode/TestingCourse/customer/CustomerRepositoryTest.java
@@ -3,11 +3,13 @@ package com.leoCode.TestingCourse.customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
 class CustomerRepositoryTest {
@@ -34,4 +36,95 @@ class CustomerRepositoryTest {
                     assertThat(c.getPhoneNumber()).isEqualTo("516-960-8086");
                 });
     }
+
+    @Test
+    void itShouldSelectCustomerByPhoneNumber(){
+        // Given
+        UUID id = UUID.randomUUID();
+        String name = "Leonel Barrientos";
+        String phoneNumber = "0000";
+        Customer customer = new Customer(id, name, phoneNumber);
+
+        // When
+        underTest.save(customer);
+
+        // Then
+        Optional<Customer> optionalCustomer = underTest.selectCustomerByPhoneNumber(phoneNumber);
+        assertThat(optionalCustomer).isPresent()
+                .hasValueSatisfying(c -> {
+                    assertThat(c.getId()).isEqualTo(id);
+                    assertThat(c.getName()).isEqualTo(name);
+                    assertThat(c.getPhoneNumber()).isEqualTo(phoneNumber);
+                });
+    }
+    @Test
+    void itShouldNotSelectCustomerByPhoneNumberWhenNumberDoesNotExist(){
+        // Given
+        String phoneNumber = "0000";
+
+        // When
+        Optional<Customer> optionalCustomer = underTest.selectCustomerByPhoneNumber(phoneNumber);
+
+        // Then
+        assertThat(optionalCustomer).isNotPresent();
+    }
+
+    @Test
+    void itShouldSelectCustomerByName() {
+        // Given
+        UUID id = UUID.randomUUID();
+        String name = "Leonel Barrientos";
+        String phoneNumber = "0000";
+        Customer customer = new Customer(id, name, phoneNumber);
+
+        // When
+        underTest.save(customer);
+
+        // Then
+        Optional<Customer> optionalCustomer = underTest.selectCustomerByName(name);
+        assertThat(optionalCustomer).isPresent()
+                .hasValueSatisfying(c -> {
+                    assertThat(c.getId()).isEqualTo(id);
+                    assertThat(c.getName()).isEqualTo(name);
+                    assertThat(c.getPhoneNumber()).isEqualTo(phoneNumber);
+                });
+    }
+
+    @Test
+    void itShouldNotSelectCustomerByNameIfNameDoesNotExist() {
+        // Given
+        String name = "Leonel Barrientos";
+
+        // When
+        Optional<Customer> optionalCustomer = underTest.selectCustomerByName(name);
+
+        // Then
+        assertThat(optionalCustomer).isNotPresent();
+    }
+    @Test
+    void itShouldNotSaveCustomerWhenNameIsNull() {
+        // Given
+        UUID id = UUID.randomUUID();
+        String phoneNumber = "0000";
+        Customer customer = new Customer(id, null, phoneNumber);
+
+        // When
+        // Then
+        assertThatThrownBy(() -> underTest.save(customer))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Test
+    void itShouldNotSaveCustomerWhenPhoneNumberIsNull() {
+        // Given
+        UUID id = UUID.randomUUID();
+        String name = "Leonel Barrientos";
+        Customer customer = new Customer(id, name, null);
+
+        // When
+        // Then
+        assertThatThrownBy(() -> underTest.save(customer))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
 }
