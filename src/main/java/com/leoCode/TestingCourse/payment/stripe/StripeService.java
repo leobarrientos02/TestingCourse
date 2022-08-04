9b3179c6a024195e3c333ead29465a131aa7f9db
leobarrientos02 <leobarrientos02@gmail.com>
2022-08-04 11:33:11 -0400

    ChargeCard method tested

--- src/main/java/com/leoCode/TestingCourse/payment/stripe/StripeService.java
+++ src/main/java/com/leoCode/TestingCourse/payment/stripe/StripeService.java
@@ -6,6 +6,8 @@ import com.leoCode.TestingCourse.payment.Currency;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Charge;
 import com.stripe.net.RequestOptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -13,14 +15,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@ConditionalOnProperty(
+        value = "stripe.enabled",
+        havingValue = "true"
+)
 public class StripeService implements CardPaymentCharger {
 
+    private final StripeApi stripeApi;
     private final static RequestOptions requestOptions = RequestOptions.builder()
             .setApiKey("sk_test_4eC39HqLyjWDarjtT1zdp7dc")
             .build();
+
+    @Autowired
+    public StripeService(StripeApi stripeApi) {
+        this.stripeApi = stripeApi;
+    }
+
     @Override
-    public CardPaymentCharge chargeCard(String cardSource, BigDecimal amount, Currency currency, String description) {
-        // `source` is obtained with Stripe.js; see https://stripe.com/docs/payments/accept-a-payment-charges#web-create-token
+    public CardPaymentCharge chargeCard(String cardSource,
+                                        BigDecimal amount,
+                                        Currency currency,
+                                        String description) {
         Map<String, Object> params = new HashMap<>();
         params.put("amount", amount);
         params.put("currency", currency);
@@ -28,10 +43,11 @@ public class StripeService implements CardPaymentCharger {
         params.put("description", description);
 
         try {
-            Charge charge = Charge.create(params, requestOptions);
+            Charge charge = stripeApi.create(params, requestOptions);
             return new CardPaymentCharge(charge.getPaid());
         } catch (StripeException e) {
             throw new IllegalStateException("Cannot make stripe charge", e);
         }
+
     }
 }
